2fae223b3d5b05c8ec1e4668ff0899d4317e3a3c
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2021-03-18 21:37:00 -0700

    initial commit

---
+++ cogs/autoroles.py
@@ -0,0 +1,26 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+
+
+
+class AutoRoles(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def setautorole(self, ctx, role: discord.Role):
+        pass
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        pass
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        pass
+
+        
+def setup(bot):
+	bot.add_cog(AutoRoles(bot))
