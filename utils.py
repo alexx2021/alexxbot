006991cbee6936eee257bd955c8e0b0d3acf6538
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2021-03-20 21:40:50 -0700

    fix logger and remove prints

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import discord
-from logging import Logger
+import logging
 
+logger = logging.getLogger('discord')
 
 async def get_or_fetch_channel(self, guild, channel_id):
     await self.bot.wait_until_ready()
     ch = guild.get_channel(int(channel_id))
     if ch:
-        print('get_channel')
+        #print('get_channel')
         return ch
 
     try:
@@ -14,13 +15,13 @@ async def get_or_fetch_channel(self, guild, channel_id):
     except discord.HTTPException:
         return None
     else:
-        print('fetch_channel')
+        #print('fetch_channel')
         return ch
 
 async def get_or_fetch_member(self, guild, member_id): #from r danny :)
     member = guild.get_member(int(member_id))
     if member is not None:
-        print('get_member')
+        #print('get_member')
         return member
 
     try:
@@ -28,7 +29,7 @@ async def get_or_fetch_member(self, guild, member_id): #from r danny :)
     except discord.HTTPException:
         return None
     else:
-        print('fetch_member')
+        #print('fetch_member')
         return member
 
 ########################LOGGING###########################
@@ -43,13 +44,13 @@ async def sendlog(self, guild, content):
         await self.bot.sc.execute("DELETE FROM logging WHERE log_channel = ?",(ch,))
         await self.bot.sc.commit()
         self.bot.logcache.pop(f"{guild.id}")
-        Logger.info(f'Deleted log channel b/c no perms to speak - {guild} ({guild.id})')
+        logger.info(msg=f'Deleted log channel b/c the bot did not have perms to speak - {guild} ({guild.id})')
 
 async def check_if_log(self, guild):
     try:
         check = self.bot.logcache[f"{guild.id}"]
-        print('CheckifLog True')
+        #print('CheckifLog True')
         return True
     except KeyError:
-        print('CheckifLog False')
+        #print('CheckifLog False')
         return False
