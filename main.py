eda41ea1801bf7bf2a6826210693f2ae5586c7c0
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2021-04-17 16:25:19 -0700

    edit default status

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ command_prefix= get_prefix,
 case_insensitive=True, 
 intents=intents, 
 allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False), 
-activity=Activity(name=f"alexx.lol | _help", type=ActivityType.playing)
+activity=Activity(name=f"_help", type=ActivityType.playing)
 )
 
 
